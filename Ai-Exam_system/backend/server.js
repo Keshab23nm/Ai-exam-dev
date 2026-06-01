89e17897a46dad4c1245254acce13ff8ad93b13e
Keshab23nm <keshabmukherjee277@gmail.com>
2026-06-01 22:05:52 +0530

    add 5000

--- Ai-Exam_system/backend/server.js
+++ Ai-Exam_system/backend/server.js
@@ -2,7 +2,7 @@ import { config } from "./src/config/config.js";
 import app from "./src/app.js";
 import connectdb from "./src/config/db.js";
 connectdb();
-const PORT = config.PORT;
+const PORT = config.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
