84e319b411fe6d7d378dc06c397883215a798e81
Keshab23nm <keshabmukherjee277@gmail.com>
2026-06-13 12:27:15 +0530

    otpfixed01

--- Ai-Exam_system/backend/src/utils/emailotpsend.js
+++ Ai-Exam_system/backend/src/utils/emailotpsend.js
@@ -1,12 +1,12 @@
-import nodemailer from 'nodemailer';
-import {config} from '../config/config.js';
- export const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.OTP_EMAIL,
-    pass: process.env.OTP_PASSWORD,
-  },
-});
+// import nodemailer from 'nodemailer';
+// import {config} from '../config/config.js';
+//  export const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.OTP_EMAIL,
+//     pass: process.env.OTP_PASSWORD,
+//   },
+// });
 
 
 
@@ -20,3 +20,17 @@ import {config} from '../config/config.js';
 //     pass: config.OTP_PASSWORD, // your generated app password
 //   },
 // });
+
+
+import nodemailer from 'nodemailer';
+import {config} from '../config/config.js';
+export const transporter = nodemailer.createTransport({
+  service: "Gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: config.OTP_EMAIL, // the email you used to create app password
+    pass: config.OTP_PASSWORD, // your generated app password
+  },
+});
