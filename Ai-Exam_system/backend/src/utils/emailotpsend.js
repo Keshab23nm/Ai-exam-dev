016c74e47cb0600a051ea3046cf962ba193b918b
Keshab23nm <keshabmukherjee277@gmail.com>
2026-06-16 15:05:20 +0530

    Replace Gmail with Brevo SMTP7

--- Ai-Exam_system/backend/src/utils/emailotpsend.js
+++ Ai-Exam_system/backend/src/utils/emailotpsend.js
@@ -1,20 +1,24 @@
 import nodemailer from "nodemailer";
 import { config } from "../config/config.js";
 
-console.log(config.BREVO_EMAIL);
-console.log(!!config.BREVO_SMTP_KEY);
+console.log("LOGIN:", config.BREVO_LOGIN);
+console.log("EMAIL:", config.BREVO_EMAIL);
+console.log("SMTP:", !!config.BREVO_SMTP_KEY);
 export const transporter =
 nodemailer.createTransport({
 
  host:"smtp-relay.brevo.com",
 
- port:587,
+ port:2525,
 
  secure:false,
 
- auth: {
-   user: config.BREVO_LOGIN,
+ auth:{
+   user:config.BREVO_LOGIN,
+   pass:config.BREVO_SMTP_KEY
+ },
 
-   pass: config.BREVO_SMTP_KEY
- }
+ connectionTimeout:60000,
+ greetingTimeout:60000,
+ socketTimeout:60000
 });
